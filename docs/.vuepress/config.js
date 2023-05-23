712f8f3c86ab583ca9f8307635a284127aac3709
tetraja <23142602+tetraja@users.noreply.github.com>
2023-05-23 00:36:26 -0400

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,9 @@
-import { defineUserConfig } from 'vuepress'
+import { defineUserConfig } from "vuepress"
 
 export default defineUserConfig({
-  lang: 'en-US',
-  title: 'FFXIV Info',
-  description: 'Dump of guides and info for ffxiv',
-  head: [['link', { rel: 'icon', href: '../images/14.png' }]],
+  lang: "en-US",
+  title: "FFXIV Info",
+  description: "Dump of guides and info for ffxiv",
+  base: "/xiv/",
+  head: [["link", { rel: "icon", href: "../images/14.png" }]],
 })
